2de0f86484c34536da37846e3b899926d055818e
Nicolas Matthew Vinson Wauquiez <nmvinson@gmail.com>
2016-06-23 19:54:34 -0400

    (feat) Expose route handlers for testing

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 var express = require('express');
-
 var app = express();
 
 require('./config/middleware.js')(app, express);
-// require('./config/routes.js')(app, express);
+require('./config/routes.js')(app, express);
+
+// Uncomment for testing server routes
+require('./config/worker.js').productRankByState('NY');
+require('./config/worker.js').birthsInRange('bank', 2015);
 
 var port = process.env.PORT || 3333;
 var server = app.listen(port, function() {
   console.log('Server listening on port: ' + port);
+  console.log(process.env.PORT, typeof ''+process.env.KEY);
 });
 
 module.exports = {
