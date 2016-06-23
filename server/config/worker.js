410d6a25bc99d99c938b7e4269c2e4da597da7d4
Nicolas Matthew Vinson Wauquiez <nmvinson@gmail.com>
2016-06-23 19:55:48 -0400

    (feat) Implement basic database queries

---
+++ server/config/worker.js
@@ -0,0 +1,58 @@
+var request = require('request-promise');
+
+// Data sources - json
+var baseComplaints = 'https://data.consumerfinance.gov/resource/jhzv-w97w.json';
+var baseCensus = 'https://api.census.gov/data/2015/pep/';
+
+// include app token in requests
+var optionA = {
+  url: baseComplaints,
+  headers: {
+    'X-App-Token': process.env.TOKEN
+  }
+};
+
+///// QUERIES to Consumer Complaints DB /////////
+
+// inputs: 2 CHAR uppercase string i.e. 'NY' -> output: most complained about product
+exports.productRankByState = function(state) {
+  request(baseComplaints + '?state=' + state + '&' + '$select=product,count(issue)&$group=product')
+    .then(function(data) {
+
+      // pluck most complained about product
+      var max = JSON.parse(data)
+        .reduce(function(a,b) {
+          return +a.count_issue > +b.count_issue ? a: b;
+        }, {count_issue:0});
+
+      console.log('send product to client:', max);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+};
+
+
+///// QUERIES to US Census DB /////////////////
+
+// inputs: bank, year -> output: total number of people born in cities
+exports.birthsInRange = function(bank, year) {
+
+  // map year of change to periods for API endpoint
+  var periods = {
+    2011: 2,
+    2012: 3,
+    2013: 4,
+    2014: 5,
+    2015: 6
+  };
+
+  // first find cities where bank had Complaints
+
+  // then find number of people born in those cities
+  request(baseCensus + 'components?get=BIRTHS,GEONAME&for=state:06&PERIOD=' + periods[year] + '&key=' + process.env.KEY)
+    .then(function(data) {
+      var births = JSON.parse(data);
+      console.log('births in range data!', births);
+    });
+};
