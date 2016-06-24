8539d05f3062477bee600eabd3332a5e8b692cc2
Nicolas Matthew Vinson Wauquiez <nmvinson@gmail.com>
2016-06-24 15:59:16 -0400

    (refactor) Clear up server logs

--- server/index.js
+++ server/index.js
@@ -7,12 +7,12 @@ require('./config/routes.js')(app, express);
 // Uncomment for testing server routes
 // require('./config/worker.js').productRankByState('NY');
 // require('./config/worker.js').birthsInRange('Bank of America', 2014);
-require('./config/worker.js').stateByProduct(0, 'Mortgage', 2014);
+// require('./config/worker.js').stateByProduct(0, 'Mortgage', 2014);
 
 var port = process.env.PORT || 3333;
 var server = app.listen(port, function() {
   console.log('Server listening on port: ' + port);
-  console.log(process.env.PORT, typeof ''+process.env.KEY);
+  console.log('Census API Key:', process.env.KEY);
 });
 
 module.exports = {
