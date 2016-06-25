5e678aa69ed1787247199a44a22f235452baa124
Nicolas Matthew Vinson Wauquiez <nmvinson@gmail.com>
2016-06-25 01:30:33 -0400

    (feat) Include Tabs component

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Tabs from './components/Tabs.jsx';
 
 class Main extends React.Component {
   render() {
-    return <div>Hello world</div>
+    return <Tabs />
   }
 }
 
