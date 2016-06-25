46806ae3fd1d9f5a5fed21ef6d592c9a82ec1882
Nicolas Matthew Vinson Wauquiez <nmvinson@gmail.com>
2016-06-25 01:31:16 -0400

    (feat) Scaffold basic components

---
+++ client/app/components/plot/Plot.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Tabs } from 'react-bootstrap';
+import { Tab } from 'react-bootstrap';
+
+export default class Plot extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    
+    return <div></div>
+  }
+
+}
