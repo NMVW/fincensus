75cac24c4c3f23e45812a1fc33f5c0b71cc37450
Nicolas Matthew Vinson Wauquiez <nmvinson@gmail.com>
2016-06-25 15:42:32 -0400

    (style) Align jumbotron

--- client/app/components/Banner.jsx
+++ client/app/components/Banner.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Jumbotron } from 'react-bootstrap';
+import { Jumbotron, Image } from 'react-bootstrap';
+
+let centerText = {
+  textAlign: 'center'
+};
 
 export default class Banner extends React.Component {
   
   render() {
     return (
       <Jumbotron>
-        <h1>Fincensus</h1>
-        <p>Showing how banks shape a country</p>
+        <h1 style={centerText}>$ Fincensus $</h1>
+        <p style={centerText}>How banks shape a country</p>
       </Jumbotron>
     )
   } 
