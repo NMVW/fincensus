3893b0df68edbf2eb2d3594bb3117f1311221bf9
Nicolas Matthew Vinson Wauquiez <nmvinson@gmail.com>
2016-06-26 01:21:56 -0400

    (fix) Maintain reference to store variables
    * States, years are API requirements for stagnant US Census DB

--- client/app/data_flow/REDUCERS.jsx
+++ client/app/data_flow/REDUCERS.jsx
@@ -1,4 +1,17 @@
 import { SET_ACTIVE, UPDATE_PARAMS, LOAD_PARAMS, UPDATE_QUERY, LOAD_QUERY } from './ACTIONS.jsx';
+
+// API-valid states list
+const States = [
+  'AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID',
+  'IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS',
+  'MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK',
+  'OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV',
+  'WI','WY'
+];
+
+// API-valid years list
+const Years = [2011,2012,2013,2014,2015];
+
 //===================================================
 // REDUCER for APP component
 import { combineReducers } from 'redux';
@@ -48,66 +61,3 @@ const fincensusApp = combineReducers({
 })
 
 export default fincensusApp;
-
-// API-valid states list
-const States = [
-  'AL',
-  'AK',
-  'AZ',
-  'AR',
-  'CA',
-  'CO',
-  'CT',
-  'DE',
-  'FL',
-  'GA',
-  'HI',
-  'ID',
-  'IL',
-  'IN',
-  'IA',
-  'KS',
-  'KY',
-  'LA',
-  'ME',
-  'MD',
-  'MA',
-  'MI',
-  'MN',
-  'MS',
-  'MO',
-  'MT',
-  'NE',
-  'NV',
-  'NH',
-  'NJ',
-  'NM',
-  'NY',
-  'NC',
-  'ND',
-  'OH',
-  'OK',
-  'OR',
-  'PA',
-  'RI',
-  'SC',
-  'SD',
-  'TN',
-  'TX',
-  'UT',
-  'VT',
-  'VA',
-  'WA',
-  'WV',
-  'WI',
-  'WY'
-];
-
-// API-valid years list
-const Years = [
-  2011,
-  2012,
-  2013,
-  2014,
-  2015
-];
--- client/app/data_flow/STORE.jsx
+++ client/app/data_flow/STORE.jsx
@@ -1,3 +1,15 @@
+// API-valid states list
+const States = [
+  'AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID',
+  'IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS',
+  'MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK',
+  'OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV',
+  'WI','WY'
+];
+
+// API-valid years list
+const Years = [2011,2012,2013,2014,2015];
+//==========================================
 // Create and configure app store
 import { createStore, compose } from 'redux'
 import fincensusApp from './REDUCERS.jsx'
@@ -19,69 +31,3 @@ const store = createStore(fincensusApp, { fincensusReducer: initialState }, comp
 );
 
 export default store;
-
-// TODO: migrate these query bounds into an API endpoint for client-side initialization
-// (remove client-side API constraints)
-
-// API-valid states list
-const States = [
-  'AL',
-  'AK',
-  'AZ',
-  'AR',
-  'CA',
-  'CO',
-  'CT',
-  'DE',
-  'FL',
-  'GA',
-  'HI',
-  'ID',
-  'IL',
-  'IN',
-  'IA',
-  'KS',
-  'KY',
-  'LA',
-  'ME',
-  'MD',
-  'MA',
-  'MI',
-  'MN',
-  'MS',
-  'MO',
-  'MT',
-  'NE',
-  'NV',
-  'NH',
-  'NJ',
-  'NM',
-  'NY',
-  'NC',
-  'ND',
-  'OH',
-  'OK',
-  'OR',
-  'PA',
-  'RI',
-  'SC',
-  'SD',
-  'TN',
-  'TX',
-  'UT',
-  'VT',
-  'VA',
-  'WA',
-  'WV',
-  'WI',
-  'WY'
-];
-
-// API-valid years list
-const Years = [
-  2011,
-  2012,
-  2013,
-  2014,
-  2015
-];
