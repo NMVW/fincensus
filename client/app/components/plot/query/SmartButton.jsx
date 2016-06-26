4b0a490b14f0fbc07b2611449bbf06791d6abe50
Nicolas Matthew Vinson Wauquiez <nmvinson@gmail.com>
2016-06-26 16:58:19 -0400

    (refactor) Let smart button handle query construction

--- client/app/components/plot/query/SmartButton.jsx
+++ client/app/components/plot/query/SmartButton.jsx
@@ -3,6 +3,11 @@ import { connect } from 'react-redux';
 import { Button } from 'react-bootstrap';
 import { loadQuery } from '../../../data_flow/ACTIONS.jsx';
 
+// Exposed API Endpoints
+// /api/states/:state/:rank
+// /api/population/:bank/:year
+// /api/growth/:year/:rank/:product
+
 class SmartButton extends React.Component {
   constructor(props) {
     super(props);
@@ -11,7 +16,25 @@ class SmartButton extends React.Component {
   
   _handleClick() {
     console.log('do i have the query path?', this.props.build);
-    this.props.dispatch(loadQuery(this.props.build));
+    let query = {};
+    let sendQuery = this.props.dispatch;
+    console.log('sendQuery',sendQuery);
+    switch (this.props.active) {
+      case 'STATES':
+        console.log('before concatenating',query);
+        query.path = 'states/' + this.props.build.STATE + '/' + this.props.build.RANK
+        console.log('after concatenating',query);
+        return sendQuery(loadQuery(query));
+      case 'GROWTH':
+        query.path = 'growth/' + this.props.build.YEAR + '/' + this.props.build.RANK + '/' + this.props.build.PRODUCT
+        return sendQuery(loadQuery(query));
+      default:
+        // 'POPULATION'
+        query.path = 'population/' + this.props.build.BANK + '/' + this.props.build.YEAR
+        return sendQuery(loadQuery(query));
+    }
+    
+    // this.props.dispatch(loadQuery(this.props.build));
   }
   
   render() {
@@ -34,6 +57,7 @@ const mapStateToProps = (store) => {
   return {
     query: store.fincensusReducer.query,
     fetching: store.fincensusReducer.fetching,
+    active: store.fincensusReducer.active
   };
 };
 
