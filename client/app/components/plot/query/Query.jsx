d9ff69271ff19b608b7651eb9205daa2b2f6d1c1
Nicolas Matthew Vinson Wauquiez <nmvinson@gmail.com>
2016-06-26 02:16:23 -0400

    (feat) Implement state-based query menu options

--- client/app/components/plot/query/Query.jsx
+++ client/app/components/plot/query/Query.jsx
@@ -20,16 +20,48 @@ export default class Query extends React.Component {
   }
 
   render() {
+    let self = this;
+    console.log(this.props.params)
+    console.log(this.props.params.map((param) => {
+      <FormGroup controlId={"formControlsSelect" + param}>
+              <ControlLabel>{ param }</ControlLabel>
+              <FormControl componentClass="select" placeholder="select">
+                <option value ={param}>param</option>
+              </FormControl>
+      </FormGroup>
+    });
+    let el = this.props.params.map((param) => {
+            <FormGroup controlId="formControlsSelect">
+              <ControlLabel>{ param }</ControlLabel>
+              <FormControl componentClass="select" placeholder="select">
+                if (param === 'STATE') {
+                  this.props.states.map((state) => {
+                    return <option key={state} value={state}>state</option>
+                  })
+                } else if (param === 'YEAR') {
+                  this.props.years.map((year) => {
+                    return <option key={year} value={year}>year</option>
+                  })
+                } else if (param === 'BANK') {
+                  this.props.banks.map((bank) => {
+                    return <option key={bank} value={bank}>bank</option>
+                  })
+                } else if (param === 'PRODUCT') {
+                  this.props.products.map((product) => {
+                    return <option key={product} value={product}>product</option>
+                  })
+                } else {
+                  [1,2,3,4,5,6,7,8,9,10].map((rank) => {
+                    return <option key={rank} value={rank}>rank</option>
+                  })
+                }
+              </FormControl>
+            </FormGroup>
+          });
+    console.log('this is el',el);
     return (
-      
       <form>
-        <FormGroup controlId="formControlsSelect">
-          <ControlLabel>{ this.props.active }</ControlLabel>
-          <FormControl componentClass="select" placeholder="select">
-            <option value={this.props.active}>{this.props.active}</option>
-            <option value={this.props.params}>{this.props.params}</option>
-          </FormControl>
-        </FormGroup>
+        {el}
         <Button type="submit" bsStyle="success">
           Show Me { this.props.active }
         </Button>
