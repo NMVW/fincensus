28b840bf7189b120fe542da4cc4d95e4b5244c13
Nicolas Matthew Vinson Wauquiez <nmvinson@gmail.com>
2016-07-10 19:15:58 -0400

    (fix) 5/7 bank model build
    * has m:m with state model, 1:m with complaint model

--- server/db/seed/bank.seed.js
+++ server/db/seed/bank.seed.js
@@ -3,22 +3,27 @@ var complaintsRequest = require('./utils').complaintsRequest;
 var baseComplaints = 'https://data.consumerfinance.gov/resource/jhzv-w97w.json?';
 var STATES = require('./utils').stateToFips;
 
-function initBanksTable(Bank) {
+function initBanksTable(Bank, State) {
   var promisedTopBanks = [];
   
   for (var state in STATES) {
     
     (function(state) {
       
-      // get THE TOP 50 MOST COMPLAINED ABOUT banks in each state consumer complaints DB
+      // get THE TOP 100 MOST COMPLAINED ABOUT banks 
+      // in each state from consumer complaints DB
       promisedTopBanks = promisedTopBanks.concat(
-        complaintsRequest(baseComplaints + '&$select=company,count(issue)&$group=company&state=' + state + '&$order=-count_issue&$limit=100')
+        complaintsRequest(baseComplaints + '$select=company,count(issue)&$group=company&state=' + state + '&$order=-count_issue&$limit=100')
           .then(function(data) {
             var banks = JSON.parse(data);
             var promisedBanks = banks.map(function(bank) {
               return Bank.create({name: bank.company})
                 .then(function(bank) {
-                  bank.addState(state);
+                  // find state model to assign relationship with bank
+                  return State.findAll({where: {capital: state}})
+                    .then(function(stateModel) {
+                      return bank.addState(stateModel[0]);
+                    });
                 });
             });
             console.log('Banks initialized.');
