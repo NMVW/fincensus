3353c9281be5c8c15b1feb79ab03aca0d517f364
Nicolas Matthew Vinson Wauquiez <nmvinson@gmail.com>
2016-07-10 19:11:07 -0400

    (fix) 1/7 model loading
    * contains 1:m relationship with complaint model

--- server/db/seed/product.seed.js
+++ server/db/seed/product.seed.js
@@ -1,12 +1,15 @@
 // populate Products table from Consumer Complaints DB API
-var request = require('request-promise');
+var complaintsRequest = require('./utils').complaintsRequest;
 var baseComplaints = 'https://data.consumerfinance.gov/resource/jhzv-w97w.json?';
 
 function initProductsTable(Product) {
+  
   // get all products from consumer complaints DB
-  return request(baseComplaints + '$select=product&$group=product')
+  return complaintsRequest(baseComplaints + '$select=product&$group=product')
     .then(function(data) {
       var products = JSON.parse(data);
+      
+      // create promise chain of product models
       var promisedProducts = products.map(function(product) {
         return Product.create({name: product.product});
       });
@@ -16,6 +19,7 @@ function initProductsTable(Product) {
     .catch(function(err) {
       console.log('Products not initialized properly!');
     });
+    
 };
 
 exports.initProductsTable = initProductsTable;
