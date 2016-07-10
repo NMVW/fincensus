26cc740175e8007230f9d0f85d93c1860229cd22
Nicolas Matthew Vinson Wauquiez <nmvinson@gmail.com>
2016-07-10 19:14:27 -0400

    (fix) 3/7 submission model build
    * has a 1:m relationship with complaint model

--- server/db/seed/submission.seed.js
+++ server/db/seed/submission.seed.js
@@ -3,26 +3,23 @@ var complaintsRequest = require('./utils').complaintsRequest;
 var baseComplaints = 'https://data.consumerfinance.gov/resource/jhzv-w97w.json?';
 
 function initSubmissionsTable(Submission) {
-  var promisedSubmissions = [];
       
-  // get all submissions
-  promisedSubmissions = promisedSubmissions.concat(
-    complaintsRequest(baseComplaints + '$select=submitted_via,count(submitted_via)&$group=submitted_via')
-      .then(function(data) {
-        var submissions = JSON.parse(data);
-        
-        var promisedSubmissions = submissions.map(function(submission) {
-          return Submission.create({via:submissions.submitted_via});
-        });
-        console.log('submissions initialized.');
-        return Promise.all(promisedSubmissions);
-      })
-      .catch(function(err) {
-        console.log('submissions not initialized properly!');
-      })
-  );
+  // get all submissions from Consumer Complaints DB
+  return complaintsRequest(baseComplaints + '$select=submitted_via,count(submitted_via)&$group=submitted_via')
+    .then(function(data) {
+      var submissions = JSON.parse(data);
+      
+      // create promise chain of submissions
+      var promisedSubmissions = submissions.map(function(submission) {
+        return Submission.create({via:submission.submitted_via});
+      });
+      console.log('Submissions initialized.');
+      return Promise.all(promisedSubmissions);
+    })
+    .catch(function(err) {
+      console.log('Submissions not initialized properly!');
+    })
     
-  return Promise.all(promisedSubmissions);
 };
 
 exports.initSubmissionsTable = initSubmissionsTable;
