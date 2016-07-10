61872ca4f176b863cf09af49b8148b167caf1a12
Nicolas Matthew Vinson Wauquiez <nmvinson@gmail.com>
2016-07-10 19:17:29 -0400

    (fix) 6/7 population model build
    * has a m:1 relationship with state model

--- server/db/seed/complaint.seed.js
+++ server/db/seed/complaint.seed.js
@@ -2,21 +2,22 @@
 var complaintsRequest = require('./utils').complaintsRequest;
 var baseComplaints = 'https://data.consumerfinance.gov/resource/jhzv-w97w.json?';
 
-function initComplaintsTable(Complaint, offset) {
+function initComplaintsTable(Complaint, State, Bank, Product, Submission, Issue, offset) {
   var promisedComplaints = [];
+  offset = offset || 0;
       
   // get the first 10,000 complaints
   promisedComplaints = promisedComplaints.concat(
-    complaintsRequest(baseComplaints + '$select=*&limit=10000&$order=date_sent_to_company&$offset=' + offset)
+    complaintsRequest(baseComplaints + '$select=*&$order=date_sent_to_company&$limit=100&$offset=' + offset)
       .then(function(data) {
         var complaints = JSON.parse(data);
         
-        var promisedComplaints = complaints.map(function(complaint) {
+        var promisedStateComplaints = complaints.map(function(complaint) {
           
           (function() {
             
             var compl = {
-              cid: compalaint.complaint_id,
+              cid: complaint.complaint_id,
               date: complaint.date_sent_to_company
             };
             
@@ -25,24 +26,44 @@ function initComplaintsTable(Complaint, offset) {
             var state = complaint.state;
             var via = complaint.submitted_via;
             var bank = complaint.company;    
-            
+
             return Complaint.create(compl)
               .then(function(complaint) {
-                complaint.setState(state);
-                complaint.setBank(bank);
-                complaint.setProduct(product);
-                complaint.setSubmission(via);
-                complaint.setIssue(issue);
+                var promisedAssoc = [];
+                
+                promisedAssoc.push(State.findAll({where: {capital: state}})
+                  .then(function(state) {
+                    console.log('found state:', state);
+                    return complaint.setState(state[0]);
+                  })
+                );                
+                promisedAssoc.push(Bank.findAll({where: {name: bank}})
+                  .then(function(bank) {
+                    return bank ? complaint.setBank(bank[0]): complaint.setBank('Not in dataset');
+                  }));  
+                promisedAssoc.push(Product.findAll({where: {name: product}})
+                  .then(function(product) {
+                    return complaint.setProduct(product[0]);
+                  }));
+                promisedAssoc.push(Submission.findAll({where: {via: via}})
+                  .then(function(sub) {
+                    return complaint.setSubmission(sub[0]);
+                  }));
+                promisedAssoc.push(Issue.findAll({where: {name: issue}})
+                  .then(function(issue) {
+                    return complaint.setIssue(issue[0]);
+                  }));
+                return Promise.all(promisedAssoc);
               });
               
           })();
           
         });
-        console.log('Complaints initialized.');
-        return Promise.all(promisedComplaints);
+        console.log('State Complaints initialized.');
+        return Promise.all(promisedStateComplaints);
       })
       .catch(function(err) {
-        console.log('Complaints not initialized properly!');
+        console.log('State Complaints not initialized properly!', err);
       })
   );
     
